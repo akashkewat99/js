16c9eeafddab746a9e6e9d881e920b4a4ec8c7c9
akashkewat99 <akashkewat99@gmail.com>
2024-08-05 22:17:47 +0530

    summary heap and Stack

---
+++ conversionOperation.js
@@ -0,0 +1,79 @@
+let score = 33 
+let score1 = "33 "
+// console.log(typeof score);
+// console.log(typeof (score1));
+
+
+// let score2=Number(score1)
+// console.log(typeof score2)
+
+/*
+"33" : 33
+33abc : NaN
+true : 1
+false : 0
+
+*/
+
+
+// let isLogin = ""
+
+// let booleanIsLogin = Boolean(isLogin)
+// console.log(booleanIsLogin)
+
+
+/*
+1 : true, 0: false
+"": false
+" ": true
+"akash": true //means not empty
+*/
+
+
+
+// ***************Operations***************//
+
+// let value = 3;
+// let negValue = -value
+// console.log(negValue);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true)
+// console.log(+"")
+
+
+let counter = 100;
+
+// ++counter
+// console.log(counter)
+
+counter++
+console.log(counter)
+
+/*
+Prefix increment
+
+let x = 3;
+const y = ++x;
+
+    x is 4; 
+    y is 4
+*/
+
+/*
+Postfix increment
+
+let x = 3;
+const y = x++;
+
+ x is 4; 
+ y is 3
+*/
+
+
---
+++ dataTypesSummary.js
@@ -0,0 +1,44 @@
+// Primitive
+
+//7 types : String , Number , Boolean, Null: Empty, undefiend : undefiend , symbol: make unique, bigint
+
+// ************************************************************
+
+//Reference / Non-Primitive
+
+//Array, Objects, functions
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (primitive)  , Heap (non-primitive)
+
+// let myold = "akash1999";
+// let newValue = myold;
+
+// newValue = "logN";
+
+// console.log(myold)
+// console.log(newValue);
+
+// -----------------------------------
+
+let userOne = {
+    email : "user@google.com",
+    upi : "akkash@tbl"
+}
+console.log("userOne", userOne)
+
+let userTwo =userOne;
+console.log("userTwo", userTwo)
+
+userOne.email = "ba@gmail.com"
+
+console.log(userOne)
+console.log(userTwo)
+
+
+
+
+
+
