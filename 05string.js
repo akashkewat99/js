6a026418f4e9771493fee97d8421cda2408a206a
akashkewat99 <akashkewat99@gmail.com>
2024-08-05 22:53:53 +0530

    update

---
+++ 05string.js
@@ -0,0 +1,26 @@
+const name = "Akash"
+const repoCount = 10
+
+console.log(`Hello my name is ${name} and I have around ${repoCount} in git server`)
+
+
+const gameName = new String('Akash-Kevat')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameNamenegetive.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('t')); // -1 represents not exit
+console.log(gameName.indexOf('h')); 
+
+
+const newString = gameName.substring(0,7)
+console.log(newString) // cant give negetive value to start or end.
+
+
+
+const url = "akash.kevat";
+console.log(url.replace('kevat','Nishad'))
+
+console.log(gameName.split('-'));
