cdf207fbbc49dbd049b16a499e50fe9f9b49f186
akashkewat99 <akashkewat99@gmail.com>
2024-08-05 20:21:19 +0530

    learn data types

---
+++ dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //this line means treat whole code as newer version
+
+// alert(3+3) //we are using node.js not browser
+
+
+/*
+number : 2 to power 53
+bigint
+string
+boolean
+null : standalone value
+undefined:
+symbol : Unique
+*/
+
+/*
+object
+*/
+let name="Akash"
+console.log(typeof "age");
+console.log(typeof null); //object
---
+++ variables.js
@@ -0,0 +1,18 @@
+const accountId = 13333;
+let accountEmail = "akash@gmail.com"
+var acccountPassword = '886611'
+
+accountCity = 'Jaipur'
+
+
+// accountId = 4 // not allowed
+accountEmail = "adars"
+acccountPassword = '7389843'
+accountCity = 'Bengaluru'
+let accountState;
+console.log(accountId)
+console.table([accountId,accountEmail,acccountPassword,accountCity,accountState])
+
+/*
+
+*/
