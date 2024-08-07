3c849a2c0112e01ffff4447aa74434e2452e32e8
akashkewat99 <akashkewat99@gmail.com>
2024-08-07 16:23:28 +0530

    add

---
+++ basic3/02scopes.js
@@ -0,0 +1,51 @@
+// // this is '{}' scope
+
+// // not using var
+
+// var c =300
+
+// if(true){
+//     let a = 400
+//     const b = 600
+//     var c = 1000
+// }
+
+// // console.log("a",a);
+// // console.log("b",b);
+// console.log("c",c); //this will rerurn you 1000 from line10. which is not excuated yet. thats the problem with var
+
+// // END
+
+// >>>>>>>>>>>
+
+// function one(){
+//     const userName = "Akash"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(userName)
+//     }
+//     two()
+// }
+
+// one()
+
+// //END
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// //START
+
+// function addOne(num){
+//     return num+1
+// }
+// // console.log(addOne(5));
+
+// const addTwo = function(num){
+//     return num+2
+// }
+// console.log(addTwo(10));
+
+// //END
+
